9234da88af576d4150d8e7b4ad36ea3d20632c5a
neguinho237 <brunomirands237@gmail.com>
2024-09-20 20:04:04 -0300

    feat:AulasD20_09

--- AulaEstruturaDeDados/src/Aula20_09/No.java
+++ AulaEstruturaDeDados/src/Aula20_09/No.java
@@ -1,5 +1,11 @@
 package Aula20_09;
 
-public class No {
+public class No  <Tipo> {
+    private Tipo elemento;
+    private No<Tipo> proximo;
     
+    public No (Tipo elemento){
+        this.elemento = elemento;
+        this.proximo = null;
+    }
 }
