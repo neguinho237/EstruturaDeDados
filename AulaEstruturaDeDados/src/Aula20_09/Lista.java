84be58d9ac70751531b76ec3c4df699bc38ce921
neguinho237 <brunomirands237@gmail.com>
2024-09-24 20:12:16 -0300

    FinishingTheCodes

--- AulaEstruturaDeDados/src/Aula20_09/Lista.java
+++ AulaEstruturaDeDados/src/Aula20_09/Lista.java
@@ -2,6 +2,12 @@ package Aula20_09;
 
 public class Lista {
     public static void main(String[] args) {
-        int soma;
+        ListaEncadeada<String> lista = new ListaEncadeada<>();
+        
+        System.out.println("Tamanho da Lista = " + lista.getTamanho());
+
+        lista.adiciona("Cladio");
+
+        
     }
 }
